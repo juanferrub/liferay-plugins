ca11c79f638c04850b6d5ce4be93679e16b4c149
Ivica Cardic <ivica.cardic@liferay.com>
2012-07-03 11:43:00 +0200

    LPS-28356 - Service implementation

---
+++ portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/service/permission/EntryPermission.java
@@ -0,0 +1,11 @@
+package com.liferay.contacts.service.permission;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ivicac
+ * Date: 7/3/12
+ * Time: 11:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EntryPermission {
+}
